d2ecddfd141c9a3fe6c35c446bfb6483e6fecd7b
Jakub <jakubhryniewicki21@gmail.com>
2021-03-03 16:13:26 +0100

    Lab 1.2

--- venv/Lab 1.2.py
+++ venv/Lab 1.2.py
@@ -1,6 +1,6 @@
 a = float(input("Podaj pierwsza liczbe: "))
 b = float(input("Podaj druga liczbe: "))
-op = input("Podaj operator: ")
+op = input("Podaj operator matematyczny: ")
 if op == "+":
     print(a + b)
 elif op == "-":
